a0fffce2962a240002d26b2840971b8cc558c11e
ftwdjw <ftwdjw@gmail.com>
2016-09-18 10:28:29 -0500

    Initial Commit

---
+++ testSParameters/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testSParameters
+//
+//  Created by John Mac on 9/18/16.
+//  Copyright © 2016 John Wetters. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
